2549373d90efb0e7b984bd5a94b8c42785f7cee2
Ali Haider <alihaiderjaffry110@gmail.com>
2024-12-17 21:05:30 +0500

    intro

---
+++ intro/mult.ts
@@ -0,0 +1,7 @@
+const mult = (a: number , b: number ) => {
+    const result =  a *b
+    console.log(`Multiplied ${a} amd ${b}, the result is ${result}`);
+}
+
+a : number = Number(process.argv[2])
+b : number = Number(process.argv[3])
